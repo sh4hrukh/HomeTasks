bb6c4a75f60c645b7157edc0cbf9ba0b2cc9ef5a
Shahrukh Shaikh <shahrukh_shaikh@epam.com>
2021-01-20 13:39:53 +0530

    Adding requested changes

--- basic-mathematical-operations.js
+++ basic-mathematical-operations.js
@@ -1,11 +1,16 @@
-function basicOp(operation, value1, value2)
-{
-  if( operation === '+')
-    return value1+value2;
-  else if( operation === '-')
-    return value1-value2;
-  else if( operation === '*')
-    return value1*value2;
-  else if( operation === '/')
-    return value1/value2;
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+      break;
+    case "-":
+      return value1 - value2;
+      break;
+    case "*":
+      return value1 * value2;
+      break;
+    case "/":
+      return value1 / value2;
+      break;
+  }
 }
--- transportation-on-vacation.js
+++ transportation-on-vacation.js
@@ -1,7 +1,7 @@
 function rentalCarCost(d) {
     if(d>6)
-      return d*40-50;
+      return (d*40)-50;
     if(d>2)
-      return d*40-20;
+      return (d*40)-20;
     return d*40;
   }
